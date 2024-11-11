e3ff0dd7ba993ed65613b30ba8e84cdacb60807f
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-11-11 17:58:38 +0100

    Update

--- demo-catalog/templates/spring-boot-ai-service/base/src/main/java/${{values.basePackageDir}}/Application.java
+++ demo-catalog/templates/spring-boot-ai-service/base/src/main/java/${{values.basePackageDir}}/Application.java
@@ -6,7 +6,9 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.source.DocumentRetriever;
+import org.springframework.ai.rag.retrieval.source.VectorStoreDocumentRetriever;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -16,15 +18,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Duration;
 
 @SpringBootApplication
 public class Application {
@@ -46,18 +45,22 @@ class ChatController {
 	private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
 
 	private final ChatClient chatClient;
-	private final VectorStore vectorStore;
+	private final DocumentRetriever documentRetriever;
 
 	ChatController(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
 		this.chatClient = chatClientBuilder.build();
-		this.vectorStore = vectorStore;
+		this.documentRetriever = VectorStoreDocumentRetriever.builder()
+			.vectorStore(vectorStore)	
+			.build();
 	}
 
 	@PostMapping("/chat")
 	String chatWithDocument(@RequestBody String message) {
 		logger.info("Received user message: {}", message);
 		return chatClient.prompt()
-				.advisors(new QuestionAnswerAdvisor(vectorStore))
+				.advisors(RetrievalAugmentationAdvisor.builder()
+					.documentRetriever(documentRetriever)
+					.build())
 				.user(message)
 				.call()
 				.content();
