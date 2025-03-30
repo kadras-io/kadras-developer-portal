e31ef15a565058cd986e8bfc3d680328ee3c24aa
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-03-30 17:44:36 +0200

    build: Improve commit stage workflow

--- demo-catalog/templates/spring-boot-ai-service/base/src/test/java/${{values.basePackageDir}}/ApplicationTests.java
+++ demo-catalog/templates/spring-boot-ai-service/base/src/test/java/${{values.basePackageDir}}/ApplicationTests.java
@@ -2,8 +2,10 @@ package ${{ values.basePackage }};
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "arconia.dev.services.ollama.enabled=true")
 class ApplicationTests {
 
 	@Test
